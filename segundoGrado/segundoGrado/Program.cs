f11192309e159f274b7afd692f2968d548784e7c
KevinTerrero <94633933+KevinTerrero@users.noreply.github.com>
2023-02-08 22:52:12 -0400

    Add files via upload

---
+++ segundoGrado/segundoGrado/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundoGrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce n1");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce n2");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce n3");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            double ecuacion = (n2 * n2) - (4 * n1 * n3);
+
+            if (ecuacion < 0)
+            {
+                Console.WriteLine("Esta solución solo funciona con numeros complejos, intente otra vez.");
+
+            }
+            else if (ecuacion == 0)
+            {
+                double a = (-n2) / (2 * n1);
+                Console.WriteLine(a.ToString("0.00" ));
+            }
+            else
+            {
+                double a = (-n2 + Math.Sqrt(ecuacion)) / (2 * n1);
+                double b = (-n2 - Math.Sqrt(ecuacion)) / (2 * n1);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
